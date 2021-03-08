3022f4469182f0bfb81fe9f35698868d49858c72
Juliya246 <78745721+Juliya246@users.noreply.github.com>
2021-03-09 01:14:29 +0200

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,11 +1,11 @@
 // Вивести значення наступних колонок: номер, код, новинка, назва, ціна, сторінки
-SELECT n, cod, new, title, price, page FROM `library`;
+SELECT n, code, new, name, price, pages FROM `library`;
 
 // Вивести значення всіх колонок
 SELECT * FROM `library`;
 
 // Вивести значення колонок в наступному порядку: код, назва, новинка, сторінки, ціна, номер
-SELECT cod, title, new, page, price, n FROM `library`;
+SELECT code, name, new, pages, price, n FROM `library`;
 
 // Вивести значення всіх колонок 10 перших рядків
 SELECT * FROM `library` LIMIT 10;
@@ -23,16 +23,16 @@ SELECT * FROM `library` WHERE `new` = 'Yes' AND `price` BETWEEN 20 AND 30
 SELECT * FROM `library` WHERE `new` = 'Yes' AND `price` NOT BETWEEN 20 AND 30
 
 // Вивести всі книги з кількістю сторінок від 300 до 400 і з ціною більше 20 і менше 30
-SELECT * FROM `library` WHERE `page` BETWEEN 300 AND 400 AND `price` BETWEEN 21 AND 29
+SELECT * FROM `library` WHERE `pages` BETWEEN 300 AND 400 AND `price` BETWEEN 21 AND 29
 
 // Вивести всі книги видані взимку 2000 року
-SELECT * FROM `library` WHERE (`dat` BETWEEN '2000-01-01' AND '2000-02-29') OR (`dat` BETWEEN '2000-12-01' AND '2000-12-31')
+SELECT * FROM `library` WHERE (`date` BETWEEN '2000-01-01' AND '2000-02-29') OR (`date` BETWEEN '2000-12-01' AND '2000-12-31')
 
 // Вивести книги зі значеннями кодів 5110, 5141, 4985, 4241
-SELECT * FROM `library` WHERE `cod` = 5110 OR `cod` = 5141 OR `cod` = 4985 OR `cod` = 4241
+SELECT * FROM `library` WHERE `code` = 5110 OR `code` = 5141 OR `code` = 4985 OR `code` = 4241
 
 // Вивести книги видані в 1999, 2001, 2003, 2005 р
-SELECT * FROM `library` WHERE `dat` LIKE '1999-%-%' OR `dat` LIKE '2001-%-%' OR `dat` LIKE '2003-%-%' OR `dat` LIKE '2005-%-%'
+SELECT * FROM `library` WHERE `date` LIKE '1999-%-%' OR `date` LIKE '2001-%-%' OR `date` LIKE '2003-%-%' OR `date` LIKE '2005-%-%'
 
 // Вивести книги назви яких починаються на літери А-К
 SELECT * FROM `library` WHERE `title` BETWEEN 'А' AND 'Л'
