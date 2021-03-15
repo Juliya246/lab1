330a0b6ca5112245b2aa65ecf42291e5325f1fdc
Juliya246 <78745721+Juliya246@users.noreply.github.com>
2021-03-15 22:48:27 +0200

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,63 +1,63 @@
 // Вивести книги у яких не введена ціна або ціна дорівнює 0
-SELECT * FROM `books` WHERE `price` = 0 OR `price` = NULL
+SELECT * FROM `library` WHERE `price` = 0 OR `price` = NULL
 
 // Вивести книги у яких введена ціна, але не введений тираж
-SELECT * FROM `books` WHERE `price` <> NULL AND `tir` = NULL
+SELECT * FROM `library` WHERE `price` <> NULL AND `circulation` = NULL
 
 // Вивести книги, про дату видання яких нічого не відомо.
-SELECT * FROM `books` WHERE `dat` = NULL
+SELECT * FROM `library` WHERE `date` = NULL
 
 // Вивести книги, з дня видання яких пройшло не більше року.
-SELECT * FROM `books` WHERE YEAR(CURRENT_DATE) - YEAR(`dat`) <= 1
+SELECT * FROM `library` WHERE YEAR(CURRENT_DATE) - YEAR(`date`) <= 1
 
 // Вивести список книг-новинок, відсортованих за зростанням ціни
-SELECT * FROM `books` WHERE `new` = 'Yes' ORDER BY `price`
+SELECT * FROM `library` WHERE `new` = 'Yes' ORDER BY `price`
 
 // Вивести список книг з числом сторінок від 300 до 400, відсортованих в зворотному алфавітному порядку назв
-SELECT * FROM `books` WHERE `page` BETWEEN 300 AND 400 ORDER BY `title` DESC
+SELECT * FROM `library` WHERE `pages` BETWEEN 300 AND 400 ORDER BY `name` DESC
 
 // Вивести список книг з ціною від 20 до 40, відсортованих за спаданням дати
-SELECT * FROM `books` WHERE `price` BETWEEN 20 AND 40 ORDER BY `dat` DESC
+SELECT * FROM `library` WHERE `price` BETWEEN 20 AND 40 ORDER BY `date` DESC
 
 // Вивести список книг, відсортованих в алфавітному порядку назв і ціною по спадаючій
-SELECT * FROM `books` ORDER BY `title` ASC, `price` DESC
+SELECT * FROM `library` ORDER BY `name` ASC, `price` DESC
 
 // Вивести книги, у яких ціна однієї сторінки < 10 копійок.
-SELECT * FROM `books` WHERE `price`* 27.93 / `page` < 0.1
+SELECT * FROM `library` WHERE `price`* 27.93 / `pages` < 0.1
 
 // Вивести значення наступних колонок: число символів в назві, перші 20 символів назви великими літерами
-SELECT LENGTH(`title`), LEFT(UPPER(`title`), 20) FROM `books`
+SELECT LENGTH(`name`), LEFT(UPPER(`name`), 20) FROM `library`
 
 // Вивести значення наступних колонок: перші 10 і останні 10 символів назви прописними буквами, розділені '...'
-SELECT LOWER(CONCAT(LEFT(title, 10), '...', RIGHT(title, 10))) FROM `books`;
+SELECT LOWER(CONCAT(LEFT(name, 10), '...', RIGHT(name, 10))) FROM `library`;
 
 // Вивести значення наступних колонок: назва, дата, день, місяць, рік
-SELECT title `Назва`, dat `Дата`, DAYNAME(dat) `День`, MONTHNAME(dat) `Місяць`, YEAR(dat) `Рік` FROM `books`;
+SELECT name `Назва`, date `Дата`, DAYNAME(date) `День`, MONTHNAME(date) `Місяць`, YEAR(date) `Рік` FROM `library`;
 
 // Вивести значення наступних колонок: назва, дата, дата в форматі 'dd/mm/yyyy'
-SELECT title `Назва`, dat `Дата`, DATE_FORMAT(dat, '%d/%m/%Y') 'dd/mm/yyyy' FROM `books`;
+SELECT name `Назва`, date `Дата`, DATE_FORMAT(date, '%d/%m/%Y') 'dd/mm/yyyy' FROM `library`;
 
 // Вивести значення наступних колонок: код, ціна, ціна в грн., ціна в євро, ціна в руб.
-SELECT cod `Код`, price `Ціна`, ROUND(price*27.90,2) `Ціна в грн`, ROUND(price*0.82,2) `Ціна в євро`, ROUND(price*74.16,2) `Ціна в руб` FROM `books`;
+SELECT code `Код`, price `Ціна`, ROUND(price*27.90,2) `Ціна в грн`, ROUND(price*0.82,2) `Ціна в євро`, ROUND(price*74.16,2) `Ціна в руб` FROM `library`;
 
 // Вивести значення наступних колонок: код, ціна, ціна в грн. без копійок, ціна без копійок округлена
-SELECT cod `Код`, price `Ціна`, TRUNCATE(price*27.90, 0) `Ціна в грн без копійок`, ROUND(price*27.90, 0) `Ціна в грн округлена` FROM `books`;
+SELECT code `Код`, price `Ціна`, TRUNCATE(price*27.90, 0) `Ціна в грн без копійок`, ROUND(price*27.90, 0) `Ціна в грн округлена` FROM `library`;
 
 // Додати інформацію про нову книгу (всі колонки)
-INSERT INTO books (`n`, `cod`, `new`, `title`, `price`, `pub`, `page`, `form`, `dat`, `tir`, `them`, `kat`)
+INSERT INTO library (`n`, `code`, `new`, `name`, `price`, `publisher`, `pages`, `format`, `date`, `circulation`, `theme`, `category`)
 VALUE (239, 1632, 'Yes', 'Этюд в багровых тонах', 6.8, 'Азбука-классика', 448, '130x200/16', '2017-04-04', 500, 'Шерлок Холмс', 'Детектив');
-SELECT * FROM `books` WHERE 1
+SELECT * FROM `library` WHERE 1
 
 // Додати інформацію про нову книгу (колонки обов'язкові для введення)
-INSERT INTO books (`n`, `cod`, `title`, `price`, `pub`, `page`, `dat`, `them`, `kat`)
+INSERT INTO library (`n`, `code`, `name`, `price`, `publisher`, `pages`, `date`, `theme`, `category`)
 VALUE (240, 1633, 'Этюд в багровых тонах', 6.8, 'Азбука-классика', 448, '2017-04-04', 'Шерлок Холмс', 'Детектив');
-SELECT * FROM `books` WHERE 1
+SELECT * FROM `library` WHERE 1
 
 // Видалити книги, видані до 1990 року
-DELETE FROM `books` WHERE YEAR(dat) < 1990;
+DELETE FROM `library` WHERE YEAR(date) < 1990;
 
 // Проставити поточну дату для тих книг, у яких дата видання відсутня
-UPDATE `books` SET `dat` = CURRENT_DATE WHERE dat = NULL;
+UPDATE `library` SET `date` = CURRENT_DATE WHERE date = NULL;
 
 // Установити ознаку новинка для книг виданих після 2005 року
-UPDATE `books` SET `new` = 'Yes' WHERE YEAR(dat) >= 2005;
+UPDATE `library` SET `new` = 'Yes' WHERE YEAR(date) >= 2005;
